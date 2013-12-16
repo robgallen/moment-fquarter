c1945c0839d709eef54e54f8b9c76d61c214b1a7
Robert Allen <robgallen@gmail.com>
2013-12-16 11:32:18 +0000

    Added module/require support Added comments & version

--- moment-fquarter.js
+++ moment-fquarter.js
@@ -1,22 +1,40 @@
-!function (moment, undefined) {
+// moment-fquarter.js
+// version : 0.3
+// author : Rob Allen
+// license : MIT
+// github.com/robgallen/moment-fquarter
 
-	moment.fn.fquarter = function (startMonth) {
-		var initial = this.lang()._quarter || "Q";
-		var adjustedDate, quarter, year, nextYear;
-		startMonth = startMonth || 4; // default is April
+(function () {
+	
+	function onload(moment) {
+		moment.fn.fquarter = function (startMonth) {
+			var initial = this.lang()._quarter || "Q";
+			var adjustedDate, quarter, year, nextYear;
+			startMonth = startMonth || 4; // default is April
 
-		if (startMonth > 1) {
-			adjustedDate = this.subtract("months", startMonth - 1);
-			nextYear = "/" + adjustedDate.clone().add("years", 1).format("YY");
-		} else {
-			adjustedDate = this;
-			nextYear = "";
-		}
+			if (startMonth > 1) {
+				adjustedDate = this.subtract("months", startMonth - 1);
+				nextYear = "/" + adjustedDate.clone().add("years", 1).format("YY");
+			} else {
+				adjustedDate = this;
+				nextYear = "";
+			}
 
-		quarter = Math.ceil((adjustedDate.month() + 1.0) / 3.0);
-		year = adjustedDate.year();
+			quarter = Math.ceil((adjustedDate.month() + 1.0) / 3.0);
+			year = adjustedDate.year();
 
-		return initial + quarter + " " + year + nextYear;
-	};
+			return initial + quarter + " " + year + nextYear;
+		};
 
-}(this.moment)
+		return moment;
+	}
+
+	if (typeof define === "function" && define.amd) {
+		define("moment-fquarter", ["moment"], onload);
+	} else if (typeof module !== "undefined") {
+		module.exports = onload(require("moment"));
+	} else if (typeof window !== "undefined" && window.moment) {
+		onload(window.moment);
+	}
+	
+}).apply(this);

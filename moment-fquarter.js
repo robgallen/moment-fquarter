6bfe605fccec3f56d5f826cccb75536a8b001031
Yago Ferrer <yferrer@zephyrhealthinc.com>
2014-03-20 08:10:57 -0700

    Returning a key/value object makes it easier to work with the results: quarter, year and next year.

--- moment-fquarter.js
+++ moment-fquarter.js
@@ -5,36 +5,35 @@
 // github.com/robgallen/moment-fquarter
 
 (function () {
-	
-	function onload(moment) {
-		moment.fn.fquarter = function (startMonth) {
-			var initial = this.lang()._quarter || "Q";
-			var adjustedDate, quarter, year, nextYear;
-			startMonth = startMonth || 4; // default is April
 
-			if (startMonth > 1) {
-				adjustedDate = this.subtract("months", startMonth - 1);
+    function onload(moment) {
+        moment.fn.fquarter = function (startMonth) {
+            var adjustedDate, quarter, year, nextYear;
+            startMonth = startMonth || 4; // default is April
+
+            if (startMonth > 1) {
+                adjustedDate = this.subtract("months", startMonth - 1);
 				nextYear = "/" + adjustedDate.clone().add("years", 1).format("YY");
-			} else {
-				adjustedDate = this;
+            } else {
+                adjustedDate = this;
 				nextYear = "";
-			}
+            }
 
-			quarter = Math.ceil((adjustedDate.month() + 1.0) / 3.0);
-			year = adjustedDate.year();
+            quarter = Math.ceil((adjustedDate.month() + 1.0) / 3.0);
+            year = adjustedDate.year();
 
-			return initial + quarter + " " + year + nextYear;
-		};
+            return {quarter: quarter, year: year, nextYear: year}
+        };
 
-		return moment;
-	}
+        return moment;
+    }
 
-	if (typeof define === "function" && define.amd) {
-		define("moment-fquarter", ["moment"], onload);
-	} else if (typeof module !== "undefined") {
-		module.exports = onload(require("moment"));
-	} else if (typeof window !== "undefined" && window.moment) {
-		onload(window.moment);
-	}
-	
+    if (typeof define === "function" && define.amd) {
+        define("moment-fquarter", ["moment"], onload);
+    } else if (typeof module !== "undefined") {
+        module.exports = onload(require("moment"));
+    } else if (typeof window !== "undefined" && window.moment) {
+        onload(window.moment);
+    }
+
 }).apply(this);

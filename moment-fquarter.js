8de385ac97cb07264f355362d2a814e6e194a7ab
David Hooey <davidh@opentext.com>
2015-09-29 10:49:25 -0400

    Fix for Issue #6, remove deprecation warnings.

--- moment-fquarter.js
+++ moment-fquarter.js
@@ -8,13 +8,13 @@
 
 	function onload(moment) {
 		moment.fn.fquarter = function (startMonth) {
-			var initial = this.lang()._quarter || "Q";
+			var initial = this.local()._quarter || "Q";
 			var result = {}, adjustedDate, nextYear = null;
 			startMonth = startMonth || 4; // default is April
 
 			if (startMonth > 1) {
-				adjustedDate = this.subtract("months", startMonth - 1);
-				nextYear = adjustedDate.clone().add("years", 1);
+				adjustedDate = this.subtract(startMonth - 1, "months");
+				nextYear = adjustedDate.clone().add(1, "years");
 			} else {
 				adjustedDate = this;
 			}
